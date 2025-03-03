f9e3815f6f06283315949e46f1eaed7ba9effcfa
Beto <150013418+AlbertoDeTeresaChaves@users.noreply.github.com>
2025-03-03 17:28:40 +0100

    created the userService

---
+++ backend/todotask/src/main/java/com/todotask/demo/service/UserService.java
@@ -0,0 +1,25 @@
+package com.todotask.demo.service;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.todotask.demo.model.User;
+
+@Service
+public interface UserService {
+
+	public Iterable<User> findAll();
+	
+	public Page<User> findAll(Pageable pageable);
+	
+	public Optional<User> findById(Long id);
+	
+	public User save(User user);
+	
+	public void deleteById(Long id);
+	
+	public boolean existsByEmail(String email);
+}
---
+++ backend/todotask/src/main/java/com/todotask/demo/service/UserServiceImpl.java
@@ -0,0 +1,60 @@
+package com.todotask.demo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.todotask.demo.model.User;
+import com.todotask.demo.repository.UserRepository;
+
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true) 
+    public Iterable<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<User> findAll(Pageable pageable) {
+        return userRepository.findAll(pageable);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    @Transactional 
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+	@Override
+	public boolean existsByEmail(String email) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
