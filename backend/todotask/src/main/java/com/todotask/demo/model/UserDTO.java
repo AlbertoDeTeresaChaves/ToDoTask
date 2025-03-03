a4955a43b1b508a719e0b9e71d03418af59fa72a
Beto <150013418+AlbertoDeTeresaChaves@users.noreply.github.com>
2025-03-03 18:56:24 +0100

    added the createdAt in the UserDTO and Lombok

--- backend/todotask/src/main/java/com/todotask/demo/model/UserDTO.java
+++ backend/todotask/src/main/java/com/todotask/demo/model/UserDTO.java
@@ -1,9 +1,22 @@
 package com.todotask.demo.model;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class UserDTO {
 	
     private Long userId;
     private String username;
     private String email;
-
+    private LocalDateTime createdAt;
 }
