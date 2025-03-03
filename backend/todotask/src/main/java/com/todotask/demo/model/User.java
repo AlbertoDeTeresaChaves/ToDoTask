7951660690e34c326a032e1b1b2e1de70873d85e
Beto <150013418+AlbertoDeTeresaChaves@users.noreply.github.com>
2025-03-03 17:27:31 +0100

    created model of the user

---
+++ backend/todotask/src/main/java/com/todotask/demo/model/User.java
@@ -0,0 +1,50 @@
+package com.todotask.demo.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "password")
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Long userId;
+
+	@Column(name = "username", nullable = false, length = 50)
+	private String username;
+
+	@Column(name = "email", unique = true, nullable = false, length = 100)
+	private String email;
+
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+
+	@PrePersist
+	public void prePersist() {
+		createdAt = LocalDateTime.now();
+	}
+
+}
---
+++ backend/todotask/src/main/java/com/todotask/demo/model/UserDTO.java
@@ -0,0 +1,9 @@
+package com.todotask.demo.model;
+
+public class UserDTO {
+	
+    private Long userId;
+    private String username;
+    private String email;
+
+}
