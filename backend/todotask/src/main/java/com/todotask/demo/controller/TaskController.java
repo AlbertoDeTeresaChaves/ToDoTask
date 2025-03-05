48e9564b30edd68542a192031ffbb921c846152c
Beto <150013418+AlbertoDeTeresaChaves@users.noreply.github.com>
2025-03-05 14:39:38 +0100

    Created TaskController

---
+++ backend/todotask/src/main/java/com/todotask/demo/controller/TaskController.java
@@ -0,0 +1,48 @@
+package com.todotask.demo.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.todotask.demo.model.Task;
+import com.todotask.demo.service.TaskService;
+
+@RestController
+@RequestMapping("api/task")
+public class TaskController {
+
+	@Autowired
+	public TaskService taskService;
+	
+	@PostMapping
+	public ResponseEntity<?> create (@RequestBody Task task){
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(taskService.save(task));
+	}
+	
+	@GetMapping
+	public ResponseEntity<?> readAll (){
+		return ResponseEntity.ok(taskService.findAll());
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity<?> delete (@PathVariable(value = "id")Long taskId ){
+		
+		Optional<Task> oTask = taskService.findById(taskId);
+		
+		if(!oTask.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		taskService.deleteById(taskId);
+		return ResponseEntity.ok().build();
+	}
+}
