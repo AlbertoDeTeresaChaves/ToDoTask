c219563293d091e68cf34a725bc3079b55f88721
Beto <150013418+AlbertoDeTeresaChaves@users.noreply.github.com>
2025-03-05 15:22:56 +0100

    changed the PostMapping to add existsByEmail

--- backend/todotask/src/main/java/com/todotask/demo/controller/UserController.java
+++ backend/todotask/src/main/java/com/todotask/demo/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.todotask.demo.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,17 +23,58 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
-	
-	
+
 	@PostMapping
-	public ResponseEntity<?> create (@RequestBody User user){
+	public ResponseEntity<?> create(@RequestBody User user) {
+
+		if(userService.existsByEmail(user.getEmail())) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("El correo ya está en uso");
+		}
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user));
 	}
-	
+
 	@GetMapping
-	public ResponseEntity<?> readAll (){
-		
+	public ResponseEntity<?> readAll() {
+
 		return ResponseEntity.ok(userService.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> read(@PathVariable Long id){
+		Optional<User> OptionalUser = userService.findById(id);
+
+		if (!OptionalUser.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(OptionalUser);
+	}
+	
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> update(@PathVariable Long id, @RequestBody User user) {
+		Optional<User> OptionalUser = userService.findById(id);
+
+		if (!OptionalUser.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		OptionalUser.get().setUsername(user.getUsername());
+		OptionalUser.get().setEmail(user.getEmail());
+		OptionalUser.get().setPassword(user.getPassword());
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(OptionalUser.get()));
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+
+		Optional<User> OptionalUser = userService.findById(id);
+
+		if (!OptionalUser.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		userService.deleteById(id);
+		return ResponseEntity.ok().build();
 	}
 }
